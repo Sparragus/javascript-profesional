a6a9aad138f9637f43d94b33f07eb07b5cf2b220
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-16 10:30:47 -0500

    this

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,12 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    // new AutoPlay()
+  ],
+});
 
 const playButton = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
