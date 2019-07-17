17a42bc0a3cb3073ca17b60f8a4063e0779512b2
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-17 12:09:55 -0500

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player) {
@@ -12,6 +13,8 @@ class AutoPause {
     });
 
     observer.observe(this.player.media);
+
+    document.addEventListener('visibilitychange', this.handleVisibilityChange);
   }
 
   handleIntersection(entries) {
@@ -19,6 +22,15 @@ class AutoPause {
 
     const isVisible = entry.intersectionRatio >= this.threshold;
 
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible';
     if (isVisible) {
       this.player.play();
     } else {
