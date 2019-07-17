eedc1eab7459cff695bfad2c3b101e7d941a02ac
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-17 11:50:23 -0500

    IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay()],
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 
 const playButton = document.querySelector('#playButton');
---
+++ assets/plugins/AutoPause.js
@@ -0,0 +1,30 @@
+class AutoPause {
+  constructor() {
+    this.threshold = 0.25;
+    this.handleIntersection = this.handleIntersection.bind(this);
+  }
+
+  run(player) {
+    this.player = player;
+
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold,
+    });
+
+    observer.observe(this.player.media);
+  }
+
+  handleIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisible = entry.intersectionRatio >= this.threshold;
+
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+}
+
+export default AutoPause;
