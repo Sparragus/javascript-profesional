70ccfccbae0d2ffce7f74a5593aebedce77e0839
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-18 10:35:28 -0500

    Tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,40 @@
-console.log('Hello, TypeScript');
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number) {
-  return a + b;
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+// people.push("9000")
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(9001);
+
+// Enum
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul',
+  Amarillo = 'Amarillo',
 }
 
-const sum = add(2, 3);
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };
