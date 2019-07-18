ef38cfd4ff5bda8667293aa0736c9938f1dfad90
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-18 11:15:26 -0500

    Interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,22 +1,30 @@
-// Funciones
-function add(a: number, b: number): number {
-  return a + b;
+// Interfaces
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
 }
 
-const sum = add(4, 6);
-
-function createAdder(a: number): (number) => number {
-  return function(b: number) {
-    return b + a;
-  };
+interface Rectangulo {
+  ancho: number;
+  alto: number;
+  color?: Color;
 }
 
-const addFour = createAdder(4);
-const fourPlus6 = addFour(6);
+let rect: Rectangulo = {
+  ancho: 4,
+  alto: 6,
+  // color: Color.Rojo,
+};
 
-function fullName(firstName: string, lastName: string = 'Smith'): string {
-  return `${firstName} ${lastName}`;
+function area(r: Rectangulo): number {
+  return r.alto * r.ancho;
 }
 
-const richard = fullName('Agente');
-console.log(richard);
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function() {
+  return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+};
+
+console.log(rect.toString());
