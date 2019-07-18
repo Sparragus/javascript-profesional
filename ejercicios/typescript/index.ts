5a0296025b927f6807547a2a14563429dedaee10
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-18 10:57:51 -0500

    Funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,40 +1,22 @@
-// Boolean
-let muted: boolean = true;
-muted = false;
-
-// Números
-let age = 6;
-let numerador: number = 42;
-let denominador: number = age;
-let resultado = numerador / denominador;
-
-// String
-let nombre: string = 'Richard';
-let saludo = `Me llamo ${nombre}`;
-
-// Arreglos
-let people: string[] = [];
-people = ['Isabel', 'Nicole', 'Raul'];
-// people.push("9000")
-
-let peopleAndNumbers: Array<string | number> = [];
-peopleAndNumbers.push('Ricardo');
-peopleAndNumbers.push(9001);
-
-// Enum
-enum Color {
-  Rojo = 'Rojo',
-  Verde = 'Verde',
-  Azul = 'Azul',
-  Amarillo = 'Amarillo',
+// Funciones
+function add(a: number, b: number): number {
+  return a + b;
 }
 
-let colorFavorito: Color = Color.Amarillo;
-console.log(`Mi color favorito es ${colorFavorito}`);
+const sum = add(4, 6);
 
-// Any
-let comodin: any = 'Joker';
-comodin = { type: 'Wildcard' };
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  };
+}
 
-// Object
-let someObject: object = { type: 'Wildcard' };
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Agente');
+console.log(richard);
