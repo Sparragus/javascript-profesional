fc4305a4030719461abd455a8e66e325412c3246
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-07-19 09:21:53 -0500

    Decorator

--- ejercicios/decorator/index.ts
+++ ejercicios/decorator/index.ts
@@ -20,9 +20,35 @@ class Field {
   validate() {}
 }
 
-function RequiredFieldDecorator(field: Field) {
+function RequiredFieldDecorator(field: Field): Field {
+  let validate = field.validate;
+
+  field.validate = function() {
+    validate();
+    let value = field.input.value;
+    if (!value) {
+      field.errors.push('Requerido');
+    }
+  };
+
+  return field;
+}
+
+function EmailFieldDecorator(field: Field): Field {
+  let validate = field.validate;
+
+  field.validate = function() {
+    validate();
+    let value = field.input.value;
+
+    if (value.indexOf('@') === -1) {
+      field.errors.push('Debe ser un email');
+    }
+  };
+
   return field;
 }
 
 let field = new Field(document.querySelector('#email'));
-RequiredFieldDecorator(field);
+field = RequiredFieldDecorator(field);
+field = EmailFieldDecorator(field);
